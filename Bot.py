1741f4b49471f4ba3f3ec1bfb338ab36393194e1
Bgm.Aliy <88282582+aliyefhx@users.noreply.github.com>
2022-04-09 08:13:14 +0400

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,16 @@
+import os, logging, asyncio
+from telethon import Button
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+from telethon.tl.types import ChannelParticipantsAdmins
+
+@client.on(events.NewMessage(pattern="^/start$"))
+async def start(event):
+  await event.reply("**LegendTagger Bot**, \nDaha çox bilgi üçün **/help**'i yazın.",
+                    buttons=(
+                      [Button.url('Owner BGM', 'https://t.me/aliyefhsos'),
+                      [Button.url('Instagram', 'https://t.me/aliyefh_sos'),
+                      [Button.url('WhatsApp', 'https://t.me/@aliyefh_sos')]
+                    ),
+                    link_preview=False
+                   )
